0502cfa49f8076215d65ee30345d6d1bc9b15bee
Maggie S <Maggiesaldivia@gmail.com>
2021-02-15 18:53:01 -0800

    add a global variable as mock database.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
+//mock "database" with global variable
+function FavoritePlaces() {
+  this.locations = {};
+}
+
 
 //create location constructor
-function Location(locationName, country, food, dates) {
-  this.name = locationName;
+function Location(locationName, country, food, notes) {
+  this.locationName = locationName;
   this.country = country;
   this.food = food;
   this.notes = notes;
 }
 
 //create prototype to call on Location objects
+Location.prototype.fullName = function() {
+  return this.locationName + ", " + this.country;
+}
