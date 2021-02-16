100a7d3786b439c67ece26eca99f8bf4dd76caa9
Maggie S <Maggiesaldivia@gmail.com>
2021-02-15 18:39:00 -0800

    add empty files, folders and started biz logic.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+//create location constructor
+function Location(locationName, country, food, dates) {
+  this.name = locationName;
+  this.country = country;
+  this.food = food;
+  this.notes = notes;
+}
+
+//create prototype to call on Location objects
